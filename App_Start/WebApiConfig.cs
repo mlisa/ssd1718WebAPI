3a60de6b6de8895da5c9ac5dc52f595535ecb9bb
Lisa Mazzini <lisa.mazzini@studio.unibo.it>
2017-11-13 10:38:33 +0100

    initial commit

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http.Headers;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+                        // per far restituire json e non xml
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ Controllers/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using System.Web.Http;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class OrdiniController : ApiController
+    {
+
+        [HttpGet]                // in esecuzione solo con un get dal client
+        [ActionName("GetAllOrders")]   // nome del metodo esposto nella API
+        public string GetAllOrders()
+        {
+            string connString;
+            List<ordini> lstOrdini = null;
+
+            connString = ConfigurationManager.ConnectionStrings["SQLiteConn"].ConnectionString;
+           
+            return JsonConvert.SerializeObject(lstOrdini);
+        }
+    }
+}
