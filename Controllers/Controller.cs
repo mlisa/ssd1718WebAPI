19b311a1382e4141855924e6174bb8197423473c
Lisa Mazzini <lisa.mazzini@studio.unibo.it>
2017-11-24 15:49:10 +0100

    get instance from db

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -15,7 +15,36 @@ namespace WebApplication1.Controllers
         string connString = ConfigurationManager.ConnectionStrings["SQLiteConn"].ConnectionString;
         string factory = "System.Data.SQLite";
 
+        public class Clienti
+        {
+            public int id;
+            public int req;
+            public int mag;
+        }
 
+        public class Magazzini
+        {
+            public int id;
+            public int node_id;
+            public int cap;
+        }
+        public class Costi
+        {
+            public int id;
+            public int mag;
+            public int cli;
+            public int cost;
+        }
+
+        public class Instance
+        {
+            public string name;
+            public int numcustomers;
+            public int numfacilities;
+            public int[,] cost;
+            public int[,] req;
+            public int[] cap;
+        }
         [HttpGet]                // in esecuzione solo con un get dal client
         [ActionName("GetAllClients")]   // nome del metodo esposto nella API
         public string GetAllClients()
@@ -70,9 +99,54 @@ namespace WebApplication1.Controllers
 
         public string getGAPInstance(int id)
         {
-            string res = "non trovato";
-            
-            return res;
+            //string res = "non trovato";
+            string queryText = "SELECT * FROM clienti";
+            string s = m.readTableViaF(connString, queryText, factory);
+            Clienti[] clienti = JsonConvert.DeserializeObject<Clienti[]>(s);
+
+            string queryText1 = "SELECT * FROM magazzini";
+            string s1 = m.readTableViaF(connString, queryText1, factory);
+            Magazzini[] magazzini = JsonConvert.DeserializeObject<Magazzini[]>(s1);
+
+            string queryText2 = "SELECT * FROM costi";
+            string s2 = m.readTableViaF(connString, queryText2, factory);
+            Costi[] costi = JsonConvert.DeserializeObject<Costi[]>(s2);
+
+
+            Instance instance = new Instance();
+
+            instance.name = "fromDB";
+            //La tabella contiene gli ordini di ogni cliente su ogni magazzino:
+            instance.numcustomers = clienti.Length/magazzini.Length;
+            instance.numfacilities = magazzini.Length;
+            int[,] cost = new int[instance.numfacilities, instance.numcustomers];
+            for (int i = 0; i < costi.Length; i++)
+            {
+                cost[costi[i].mag - 1, costi[i].cli - 1] = costi[i].cost;
+            }
+
+            int[,] req = new int[instance.numfacilities, instance.numcustomers];
+            for (int i = 0; i < clienti.Length; i++)
+            {
+                req[clienti[i].mag - 1, clienti[i].id - 1] = clienti[i].req;
+            }
+
+            int[] cap = new int[instance.numfacilities];
+            for (int i = 0; i < magazzini.Length; i++)
+            {
+                cap[magazzini[i].id - 1] = magazzini[i].cap;
+            }
+
+            instance.cost = cost;
+            instance.req = req;
+            instance.cap = cap;
+
+
+
+            return JsonConvert.SerializeObject(instance);
         }
+
+
+
     }
 }
