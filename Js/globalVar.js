be8a48813b2b7ebf4625918f0ddb7727d5febe28
Lisa Mazzini <lisa.mazzini@studio.unibo.it>
2017-11-27 10:56:02 +0100

    Created dir with js files for optimization

---
+++ Js/globalVar.js
@@ -0,0 +1,13 @@
+﻿var n;   // num clienti
+var m;   // num server
+var c;   // matrice dei costi
+var req; // matrice delle richieste
+var cap; // vettore delle capacità
+var sol; // vettore soluzione con indici dei magazzini a cui è assegnato il client sol[j] c'è il magazzino a cui è assegnato il client j
+var solbest; // vettore migliore soluzione trovata
+var startTime, endTime, timeDiff; // tempi esecuzione
+var zub = Number.MAX_VALUE;       // costo miglior soluzione trovata: la funzione di costo si chiama Z, ub è upper bound 
+var zlbBest = Number.MAX_VALUE;   // best lower bound
+
+var jInstance;    // istanza in input
+var EPS = 0.001;
