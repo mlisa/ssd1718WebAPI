a2303ca1c8ddcd0820b302369f51cd8f29b47761
Lisa Mazzini <lisa.mazzini@studio.unibo.it>
2017-12-15 16:28:36 +0100

    cose

--- Js/globalVar.js
+++ Js/globalVar.js
@@ -3,7 +3,7 @@ var m;   // num server
 var c;   // matrice dei costi
 var req; // matrice delle richieste
 var cap = new Array(); // vettore delle capacità
-var sol; // vettore soluzione con indici dei magazzini a cui è assegnato il client sol[j] c'è il magazzino a cui è assegnato il client j
+var sol = new Array(n); // vettore soluzione con indici dei magazzini a cui è assegnato il client sol[j] c'è il magazzino a cui è assegnato il client j
 var solbest; // vettore migliore soluzione trovata
 var startTime, endTime, timeDiff; // tempi esecuzione
 var zub = Number.MAX_VALUE;       // costo miglior soluzione trovata: la funzione di costo si chiama Z, ub è upper bound 
